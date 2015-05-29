b68e27ebfb6af18072c8966982925c55d8904cc2
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-29 21:38:52 +0300

    ServiceProvider: add configs to be published by artisan

--- src/Alakkad/WorldCountriesCities/WorldCountriesCitiesServiceProvider.php
+++ src/Alakkad/WorldCountriesCities/WorldCountriesCitiesServiceProvider.php
@@ -4,31 +4,44 @@ use Illuminate\Support\ServiceProvider;
 
 class WorldCountriesCitiesServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Perform post-registration
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/countries.php' => config_path('countries.php'),
+            __DIR__.'/config/cities.php' => config_path('cities.php'),
+        ]);
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
-	}
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 
 }
