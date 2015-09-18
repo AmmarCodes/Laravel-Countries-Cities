118ad1ecd2ca217cbd73fec79079026ae64952cc
Jorge Munguia <jorge@buroburo.org>
2015-09-18 15:09:34 -0500

    same thing. integers 2nd argument is now autoincrement

--- src/Alakkad/WorldCountriesCities/migrations/2015_05_29_201147_create_countries_table.php
+++ src/Alakkad/WorldCountriesCities/migrations/2015_05_29_201147_create_countries_table.php
@@ -18,15 +18,15 @@ class CreateCountriesTable extends Migration
             $table->enum('continent', ['Asia', 'Europe', 'North America', 'Africa', 'Oceania', 'Antarctica', 'South America']);
             $table->string('region', 26);
             $table->decimal('surface_area', 10, 2);
-            $table->integer('indep_year', 4);
-            $table->integer('population', 11)->unsigned();
+            $table->integer('indep_year')->length(4);
+            $table->integer('population')->length(11)->unsigned();
             $table->decimal('life_expectancy', 3, 1);
             $table->decimal('gnp', 10, 2);
             $table->decimal('gnp_old', 10, 2);
             $table->string('local_name', 45);
             $table->string('government_form', 45);
             $table->string('head_of_state', 60);
-            $table->integer('capital', 11)->unsigned();
+            $table->integer('capital')->length(11)->unsigned();
             $table->string('code2', 2);
             $table->foreign('capital', 11)->references('id')->on('cities');
         });
